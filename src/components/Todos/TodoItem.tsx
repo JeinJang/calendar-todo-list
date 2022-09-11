6251e228b32bb1d93cdacc808f02713836ba6dcd
JeinJang <jein.jang@poomang.com>
2022-09-12 01:49:53 +0900

    feature: #4 todo form created

--- src/components/Todos/TodoItem.tsx
+++ src/components/Todos/TodoItem.tsx
@@ -1,37 +1,66 @@
+import { useState } from "react";
 import Box from "../core/Box";
 import Text from "../core/Text";
+import TodoForm from "./TodoForm";
 
 type Props = {
   id: string;
   name: string;
   time: Date;
+  todoList: any;
+  setTodoList?: any;
+  isCurrent: string;
+  setCurrent: any;
 };
 
-export default function TodoItem({id, name, time}: Props) {
+export default function TodoItem({id, name, time, todoList, setTodoList, isCurrent, setCurrent}: Props) {
+  const [isFormOpen, setFormOpen] = useState(false);
   return (
-    <Box display="flex">
-      <Text marginRight="0.5rem">
-        {`${time.getHours()}`.padStart(2, '0')} : {`${time.getMinutes()}`.padStart(2, '0')}
-      </Text>
-      <Text>{name}</Text>
-      <Box
-        type="button"
-        as="button"
-        onClick={() => {}}
-        padding="0"
-        margin="auto 0 auto auto"
-      >
-        수정
+    <>
+      <Box display="flex">
+        <Text marginRight="0.5rem">
+          {`${time.getHours()}`.padStart(2, '0')} : {`${time.getMinutes()}`.padStart(2, '0')}
+        </Text>
+        <Text>{name}</Text>
+        <Box
+          type="button"
+          as="button"
+          onClick={() => {
+            setCurrent(id);
+            setFormOpen(true);
+          }}
+          padding="0"
+          margin="auto 0 auto auto"
+        >
+          수정
+        </Box>
+        <Box
+          type="button"
+          as="button"
+          onClick={() => {
+            const newTodo = todoList.filter(
+              item => item.id !== id
+            );
+            setTodoList(newTodo)
+          }}
+          padding="0"
+          margin="auto 0 auto 1rem"
+        >
+          삭제
+        </Box>
       </Box>
-      <Box
-        type="button"
-        as="button"
-        onClick={() => {}}
-        padding="0"
-        margin="auto 0 auto 1rem"
-      >
-        삭제
-      </Box>
-    </Box>
+      {isFormOpen && isCurrent===id && (
+        <TodoForm
+          date={time}
+          time={time}
+          name={name}
+          id={id}
+          type="update"
+          todoList={todoList}
+          setTodoList={setTodoList}
+          closeForm={() => setFormOpen(false)}
+        />
+      )}
+    </>
   );
 };
--- src/components/Todos/index.tsx
+++ src/components/Todos/index.tsx
@@ -1,27 +1,32 @@
+import { useState } from "react";
 import Box from "../core/Box";
+import TodoForm from "./TodoForm";
 import TodoItem from "./TodoItem";
 import TodosHeader from "./TodosHeader";
 
-const todoList = [
-  {
-    id: '1',
-    name: '코딩 테스트 하기',
-    time: new Date()
-  },
-  {
-    id: '2',
-    name: '코딩 테스트 하기',
-    time: new Date()
-  },
-  {
-    id: '3',
-    name: '코딩 테스트 하기',
-    time: new Date()
-  }
-];
-
 export default function Todos() {
   const currentDate = new Date();
+  const [isOpen, setOpen] = useState(false);
+  const [isCurrent, setCurrent] = useState(null);
+  const [todoList, setTodoList] = useState(
+    [
+      {
+        id: '1',
+        name: '코딩 테스트 하기',
+        time: new Date()
+      },
+      {
+        id: '2',
+        name: '코딩 테스트 하기',
+        time: new Date()
+      },
+      {
+        id: '3',
+        name: '코딩 테스트 하기',
+        time: new Date()
+      }
+    ]
+  );
   
   return (
     <Box
@@ -40,9 +45,33 @@ export default function Todos() {
               id={todo.id}
               name={todo.name}
               time={todo.time}
+              todoList={todoList}
+              setTodoList={setTodoList}
+              isCurrent={isCurrent}
+              setCurrent={setCurrent}
             />
           )
         )}
+      </Box>
+      {isOpen && isCurrent==='create' && (
+        <TodoForm
+          date={currentDate}
+          type="create"
+          closeForm={() => setOpen(false)}
+          todoList={todoList}
+          setTodoList={setTodoList}
+        />
+      )}
+      <Box 
+        as="button" 
+        type="button" 
+        onClick={() => {
+          setCurrent('create');
+          setOpen(true);
+        }} 
+        marginBottom="1rem"
+      >
+        Add
       </Box>
     </Box>
   );
