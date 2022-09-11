bcc921044e626674d8b71feb67e2a27593971f7c
JeinJang <jein.jang@poomang.com>
2022-09-12 00:21:42 +0900

    feature: #3 calendar empty space edited

--- src/components/Calendar/CalendarItem.tsx
+++ src/components/Calendar/CalendarItem.tsx
@@ -2,26 +2,33 @@ import Box from "../core/Box";
 import Text from "../core/Text";
 
 type Props = {
-  date: number;
+  date?: number;
   isSelected?: boolean;
+  isDate?: boolean;
   gridColumnStart: string;
   onClick?: () => void;
 };
 
 export default function CalendarItem({
   date,
-  isSelected=true,
+  isSelected=false,
+  isDate=true,
   gridColumnStart,
   onClick
 }: Props) {
   return (
     <Box
       background={isSelected ? 'purple' : 'white'}
+      border="none"
+      textAlign="left"
       gridColumnStart={gridColumnStart}
+      {...isDate && {
+        as: 'button',
+        type: 'button',
+        onClick
+      }}
     >
-      <Text>
-        {date}
-      </Text>
+      {isDate && <Text>{date}</Text>}
     </Box>
   );
 };
--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -41,16 +41,40 @@ export default function Calendar() {
         borderRight="3px solid black"
         borderBottom="3px solid black"
       >
+        {/* 달력 빈 부분 채우는 코드 */}
+        {Array.from({
+          length: new Date(currentYear, currentMonth, 1).getDay()
+        }, (v, i) => i + 1).map(
+          val => (
+            <CalendarItem
+              key={`before ${currentMonth + 1}, ${val}`}
+              isDate={false}
+              gridColumnStart={`${val}`}
+            />
+          )
+        )}
         {/* Date에서 date부분에 0을 넣으면 이전 달의 마지막을 return */}
         {Array.from({
           length: new Date(currentYear, currentMonth + 1, 0).getDate()
         }, (v, i) => new Date(currentYear, currentMonth, i + 1)).map(
           val => (
             <CalendarItem
-              key={`${currentMonth}월 ${val.getDate()}일`}
+              key={`${currentMonth + 1}월 ${val.getDate()}일`}
               date={val.getDate()}
               isSelected={now.toLocaleDateString() === val.toLocaleDateString()}
               gridColumnStart={`${val.getDay() + 1}`}
+            />
+          )
+        )}
+        {/* 달력 빈 부분 채우는 코드 */}
+        {Array.from({
+          length: 6 - new Date(currentYear, currentMonth + 1, 0).getDay()
+        }, (v, i) => i + new Date(currentYear, currentMonth + 1, 0).getDay() + 2).map(
+          val => (
+            <CalendarItem
+              key={`after ${currentMonth + 1}, ${val}`}
+              isDate={false}
+              gridColumnStart={`${val}`}
             />
           )
         )}
